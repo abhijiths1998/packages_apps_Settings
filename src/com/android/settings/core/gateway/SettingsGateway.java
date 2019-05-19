f5f90334ca1f61c23dc992cf4cf95d37d95afb7a
Satyam Sharma <33489151+Dixzz@users.noreply.github.com>
2019-05-19 11:34:19 +0530

    Update SettingsGateway.java
    * derp

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -138,6 +138,7 @@ import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 import com.android.settings.custom.ambient.history.AmbientPlayHistory;
+import com.CTextras.settings.ExtraSettings;
 
 public class SettingsGateway {
 
@@ -265,6 +266,7 @@ public class SettingsGateway {
             ToggleBackupSettingFragment.class.getName(),
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
             AmbientPlayHistory.class.getName(),
+            ExtraSettingsActivity.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
